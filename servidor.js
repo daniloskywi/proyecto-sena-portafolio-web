64f0d2d123830ee78896002427fc1df460455305
Danilo <danilohei2002@gmail.com>
2026-06-29 17:50:20 -0500

    Se agrego de add: documentos al servidor

--- servidor.js
+++ servidor.js
@@ -6,6 +6,7 @@ const { Resend } = require('resend');
 const app = express();
 app.use(express.json());
 app.use(express.static('./Public'));
+app.use('/documentos', express.static('./documentos')); 
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
